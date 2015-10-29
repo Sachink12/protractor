aa15dab04732ccdc22f6db60e0697a8d4e1405a3
sachink12 <sachin.sit@gmail.com>
2015-10-29 10:44:04 -0400

    First Commit

---
+++ example/browsers.js
@@ -0,0 +1,21 @@
+exports.chrome = {
+	name: 'Chrome',
+	browserName: 'chrome'
+}
+
+exports.firefox = {
+	name: 'Firefox',
+	browserName: 'firefox'
+}
+
+exports.safari = {
+	name: 'Safari',
+	browserName: 'safari'
+}
+
+exports.ie9 = {
+	name: 'IE 9',
+	browserName: 'internet explorer',
+	version: '9.0',
+	tags: ['ie']
+}
---
+++ example/conf.js
@@ -0,0 +1,44 @@
+
+var browsers = require('./browsers.js')
+
+// An example configuration file.
+var config = {
+  directConnect: false,
+
+  // Capabilities to be passed to the webdriver instance.
+ 
+  baseUrl:'http://www.angularjs.org',
+  allScriptsTimeout:30000,
+  getPageTimeout:30000,
+
+  // Framework to use. Jasmine 2 is recommended.
+  framework: 'jasmine2',
+
+  // Spec patterns are relative to the current working directly when
+  // protractor is called.
+  specs: ['example_spec.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    showColors: true,
+    isVerbose: true,
+    includeStackTrace: true,
+    defaultTimeoutInterval: 300000
+  },
+  sauceUser: 'sachink12',
+  sauceKey: 'ed57321d-2274-479a-898c-158b95c97d31',
+
+
+};
+
+config.multiCapabilities = [
+
+    browsers.chrome,
+    browsers.firefox,
+    browsers.safari
+
+
+]
+ exports.config = config;   
+
+
